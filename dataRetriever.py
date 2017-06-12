2fcdecb9327295e3f258b24c78bbaba40b30fc61
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2017-06-12 14:49:00 +0200

    data is shuffled

--- dataRetriever.py
+++ dataRetriever.py
@@ -3,14 +3,25 @@
 # returnable. The second is a list of all the rows with only the 
 # last column.
 import csv
+import random
 
-def retrieveData(file_loc = "data/UCI_Credit_Card.csv"):
+def retrieveData(file_loc="data/UCI_Credit_Card.csv", randomize=True, make_floats=True):
 	x = []
 	y = []
 	with open(file_loc) as data:
-		next(data) #skip header
+		next(data) #skip header1
+		next(data) #skip header2
 		read_data = csv.reader(data)
 		for column in read_data:
-			x.append(column[1:-1])
-			y.append(column[-1])
+			if make_floats:
+				x.append(list(map(float, column[1:-1])))
+				y.append(float(column[-1]))
+			else:
+				x.append(column[1:-1])
+				y.append(column[-1])
+	if randomize:
+		combined = list(zip(x, y))
+		random.shuffle(combined)
+
+		x[:], y[:] = zip(*combined)
 	return x, y
